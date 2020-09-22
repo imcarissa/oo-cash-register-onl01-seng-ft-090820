9b67cf962bbb889baeae3bfe70aef44eaa9830ad
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-22 00:33:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,5 +8,18 @@ class CashRegister
   end
   
   def add_items(title, price, quantity = 1)
-    
+    def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
     
