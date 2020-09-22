46ce87cab22b4acb7e542f225f156119f472b400
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-22 01:08:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,13 +24,13 @@ class CashRegister
   
   def apply_discount
     if @discount > 0
-      @to_take_off = (price * discount)/100
+      @discount = (price * discount)/100
       @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
+      "After the discount, the total comes to $#{total}."
     else
-      return "There is no discount to apply."
+      "There is no discount to apply."
     end
-    
+  end
 
   def void_last_transaction
     @total -= @price
